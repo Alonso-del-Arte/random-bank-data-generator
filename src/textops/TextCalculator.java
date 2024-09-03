b629e5e253f211c768f0e10010530be1d2a991ad
Alonso del Arte <alonso.delarte@gmail.com>
2024-09-03 01:00:17 -0400

    New class

---
+++ src/textops/TextCalculator.java
@@ -0,0 +1,15 @@
+package textops;
+
+public class TextCalculator {
+
+    // TODO: Write tests for this
+    public static String leftPad(String s) {
+        return "SORRY, NOT IMPLEMENTED YET";
+    }
+
+    // TODO: Write tests for this
+    public static String rightPad(String s) {
+        return "SORRY, NOT IMPLEMENTED YET";
+    }
+
+}
