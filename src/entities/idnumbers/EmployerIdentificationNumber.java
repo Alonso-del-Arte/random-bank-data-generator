3f1128b4469318a9d8677e389197446435806fe2
Alonso del Arte <alonso.delarte@gmail.com>
2024-09-03 00:51:15 -0400

    Added serialVersionUID

--- src/entities/idnumbers/EmployerIdentificationNumber.java
+++ src/entities/idnumbers/EmployerIdentificationNumber.java
@@ -2,6 +2,8 @@ package entities.idnumbers;
 
 public class EmployerIdentificationNumber extends TaxpayerIdentificationNumber {
 
+    private static final long serialVersionUID = 17808292273192961L;
+
     // TODO: Write tests for this
     static boolean correctEINDashPlacement(String s) {
         return false;
--- src/entities/idnumbers/SocialSecurityNumber.java
+++ src/entities/idnumbers/SocialSecurityNumber.java
@@ -4,6 +4,8 @@ import java.text.DecimalFormat;
 
 public class SocialSecurityNumber extends TaxpayerIdentificationNumber {
 
+    private static final long serialVersionUID = 17808292273192962L;
+
     // TODO: Write tests for this
     static boolean correctSSNDashPlacement(String s) {
         return false;
