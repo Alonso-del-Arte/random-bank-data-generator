ee8198d0427bcc2e0b62f59602759ef1f59299ef
Alonso del Arte <alonso.delarte@gmail.com>
2024-09-03 01:05:18 -0400

    randomUnicodeChars( ) stubs

--- src/textops/TextCalculator.java
+++ src/textops/TextCalculator.java
@@ -3,6 +3,16 @@ package textops;
 public class TextCalculator {
 
     // TODO: Write tests for this
+    public static String randomUnicodeChars(int len) {
+        return "SORRY, NOT IMPLEMENTED YET";
+    }
+
+    // TODO: Write tests for this
+    public static String randomUnicodeChars(int len, Character.UnicodeBlock block) {
+        return "SORRY, NOT IMPLEMENTED YET";
+    }
+
+    // TODO: Write tests for this
     public static String leftPad(String s) {
         return "SORRY, NOT IMPLEMENTED YET";
     }
