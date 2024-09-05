f7d24de2335cb3c5c4c0cc115de4935264db658e
Alonso del Arte <alonso.delarte@gmail.com>
2024-09-05 01:20:42 -0400

    Passing randomUnicodeChars( ) rejects negative length test

--- src/textops/TextCalculator.java
+++ src/textops/TextCalculator.java
@@ -1,5 +1,10 @@
 package textops;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+
 public class TextCalculator {
 
     // TODO: Write tests for this
@@ -9,7 +14,13 @@ public class TextCalculator {
 
     // TODO: Write tests for this
     public static String randomUnicodeChars(int len, Character.UnicodeBlock block) {
-        return "SORRY, NOT IMPLEMENTED YET";
+        if (len < 0) {
+            String excMsg = "Length " + len + " is not valid";
+            throw new NegativeArraySizeException(excMsg);
+        }
+        char[] characters = new char[len];
+        Arrays.fill(characters, 'X');
+        return new String(characters);
     }
 
     // TODO: Write tests for this
@@ -20,6 +31,17 @@ public class TextCalculator {
     // TODO: Write tests for this
     public static String rightPad(String s) {
         return "SORRY, NOT IMPLEMENTED YET";
+    }
+
+    private static class BMPCharPair {
+
+        final char beginChar, endChar;
+
+        BMPCharPair(char from, char to) {
+            this.beginChar = from;
+            this.endChar = to;
+        }
+
     }
 
 }
