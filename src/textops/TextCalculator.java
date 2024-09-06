d23834695733b47ae2b94a2d020736fe8f360c61
Alonso del Arte <alonso.delarte@gmail.com>
2024-09-06 17:40:49 -0400

    Passing randomASCIIChars( ) test

--- src/textops/TextCalculator.java
+++ src/textops/TextCalculator.java
@@ -7,14 +7,29 @@ import java.util.Random;
 
 public class TextCalculator {
 
-    // TODO: Write tests for this
+    private static final Random RANDOM = new Random();
+
+    /**
+     * Gives a {@code String} of ASCII characters of specified length.
+     * @param len How long the result should be. For example, 25. May be 0, but
+     *            preferably greater.
+     * @return A {@code String} with as many characters as specified by {@code
+     * len}.
+     * @throws NegativeArraySizeException If {@code len} is negative.
+     */
     public static String randomASCIIChars(int len) {
         if (len < 0) {
             String excMsg = "Length " + len + " is not valid";
             throw new NegativeArraySizeException(excMsg);
         }
-        return "SORRY, NOT IMPLEMENTED YET";
+        char[] characters = new char[len];
+        for (int i = 0; i < len; i++) {
+            char ch = (char) (RANDOM.nextInt(95) + 32);
+            characters[i] = ch;
+        }
+        return new String(characters);
     }
+
     // TODO: Write tests for this
     public static String randomUnicodeChars(int len) {
         if (len < 0) {
