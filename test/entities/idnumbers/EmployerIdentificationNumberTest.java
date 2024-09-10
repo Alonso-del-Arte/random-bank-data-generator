878e1ce82017c53a80bfe982e749b02ae5af47be
Alonso del Arte <alonso.delarte@gmail.com>
2024-09-10 00:37:50 -0400

    Failing correctEINDashPlacement( ) test

--- test/entities/idnumbers/EmployerIdentificationNumberTest.java
+++ test/entities/idnumbers/EmployerIdentificationNumberTest.java
@@ -1,7 +1,24 @@
 package entities.idnumbers;
 
+import static currency.CurrencyAmountTest.RANDOM;
+
+import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
+import textops.TextCalculator;
+
 class EmployerIdentificationNumberTest {
+
+    @Test
+    void testCorrectEINDashPlacement() {
+        System.out.println("correctEINDashPlacement");
+        String intermediate = TextCalculator.leftPad(
+                Integer.toString(RANDOM.nextInt(1000000000)), 9, '0');
+        String s = intermediate.substring(0, 2) + '-'
+                + intermediate.substring(2);
+        String msg = "EIN " + s
+                + " should be found to have correct dash placement";
+        assert EmployerIdentificationNumber.correctEINDashPlacement(s) : msg;
+    }
 
 }
