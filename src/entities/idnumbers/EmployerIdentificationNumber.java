01147bcb8c52fa5ba90fb2258e146f271e878be4
Alonso del Arte <alonso.delarte@gmail.com>
2024-12-16 00:36:11 -0500

    Passing toString( ) test

--- src/entities/idnumbers/EmployerIdentificationNumber.java
+++ src/entities/idnumbers/EmployerIdentificationNumber.java
@@ -2,18 +2,31 @@ package entities.idnumbers;
 
 import java.io.Serial;
 
+import org.w3c.dom.Text;
+import textops.TextCalculator;
+
 public class EmployerIdentificationNumber extends TaxpayerIdentificationNumber {
 
     @Serial
     private static final long serialVersionUID = 17808292273192961L;
 
+    final int num;
+
     static boolean correctEINDashPlacement(String s) {
         return s.indexOf('-') == 2;
+    }
+
+    @Override
+    public String toString() {
+        String intermediate = TextCalculator.leftPad(Integer.toString(this.num),
+                9, '0');
+        return intermediate.substring(0, 2) + '-' + intermediate.substring(2);
     }
 
     // TODO: Write tests for this
     public EmployerIdentificationNumber(int number) {
         super(number);
+        this.num = number;
     }
 
 }
