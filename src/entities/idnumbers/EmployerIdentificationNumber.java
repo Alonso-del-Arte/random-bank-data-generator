182f158f60093b65940749d05c33b9fd0ffe9ab6
Alonso del Arte <alonso.delarte@gmail.com>
2024-12-16 00:20:48 -0500

    Passing incorrect EIN dash placement at position 1 test

--- src/entities/idnumbers/EmployerIdentificationNumber.java
+++ src/entities/idnumbers/EmployerIdentificationNumber.java
@@ -6,7 +6,7 @@ public class EmployerIdentificationNumber extends TaxpayerIdentificationNumber {
 
     // TODO: Write tests for this
     static boolean correctEINDashPlacement(String s) {
-        return s.indexOf('-') != 0;
+        return s.indexOf('-') > 1;
     }
 
     // TODO: Write tests for this
