ba02febb4aeedb144a938ce103dbfb65e8aa8ad2
Alonso del Arte <alonso.delarte@gmail.com>
2024-12-16 00:37:46 -0500

    Annotated serialVersionUID as @Serial per IntelliJ IDEA suggestion

--- src/entities/idnumbers/SocialSecurityNumber.java
+++ src/entities/idnumbers/SocialSecurityNumber.java
@@ -1,9 +1,11 @@
 package entities.idnumbers;
 
+import java.io.Serial;
 import java.text.DecimalFormat;
 
 public class SocialSecurityNumber extends TaxpayerIdentificationNumber {
 
+    @Serial
     private static final long serialVersionUID = 17808292273192962L;
 
     // TODO: Write tests for this
--- src/entities/idnumbers/TaxpayerIdentificationNumber.java
+++ src/entities/idnumbers/TaxpayerIdentificationNumber.java
@@ -1,11 +1,13 @@
 package entities.idnumbers;
 
+import java.io.Serial;
 import java.io.Serializable;
 import java.text.DecimalFormat;
 import java.text.DecimalFormatSymbols;
 
 public abstract class TaxpayerIdentificationNumber implements Serializable {
 
+    @Serial
     private static final long serialVersionUID = 17808292273192960L;
 
     // TODO: Write tests for this
