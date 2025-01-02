589c1e91b26a3c5821471152861d4fc423229f37
Alonso del Arte <alonso.delarte@gmail.com>
2025-01-01 22:49:12 -0500

    Failing choose currency with with 10000 subdivisions test

--- test/currency/CurrencyChooserTest.java
+++ test/currency/CurrencyChooserTest.java
@@ -240,4 +240,14 @@ class CurrencyChooserTest {
         assertEquals(expected, actual, message);
     }
 
+    @Test
+    public void testChooseCurrencyWith10000Divisions() {
+        int expected = 4;
+        Currency currency = CurrencyChooser.chooseCurrency(expected);
+        int actual = currency.getDefaultFractionDigits();
+        String message = "Chosen currency " + currency.getDisplayName()
+                + " should have " + expected + " default fraction digits";
+        assertEquals(expected, actual, message);
+    }
+
 }
