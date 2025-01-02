3e2047dbbf71c0f6e9623d1c209618541ec5896c
Alonso del Arte <alonso.delarte@gmail.com>
2025-01-01 20:53:31 -0500

    Constructor, function stubs

--- src/currency/CurrencyAmount.java
+++ src/currency/CurrencyAmount.java
@@ -3,4 +3,64 @@ package currency;
 import java.util.Currency;
 
 public class CurrencyAmount {
+
+    // TODO: Write tests for this
+    public long getAmountInCents() {
+        return Long.MIN_VALUE;
+    }
+
+    // TODO: Write tests for this
+    public long getUnitAmount() {
+        return Long.MAX_VALUE;
+    }
+
+    // TODO: Write tests for this
+    public short getChangeAmount() {
+        return Short.MIN_VALUE;
+    }
+
+    // TODO: Write tests for this
+    public Currency getCurrency() {
+        return Currency.getInstance("XTS");
+    }
+
+    // TODO: Write tests for this
+    public CurrencyAmount plus(CurrencyAmount addend) {
+        return this;
+    }
+
+    // TODO: Write tests for this
+    public CurrencyAmount negate() {
+        return this;
+    }
+
+    // TODO: Write tests for this
+    public CurrencyAmount minus(CurrencyAmount subtrahend) {
+        return this;
+    }
+
+    // TODO: Write tests for this
+    public CurrencyAmount times(int multiplicand) {
+        return this;
+    }
+
+    // TODO: Write tests for this
+    public CurrencyAmount divides(int divisor) {
+        return this;
+    }
+
+    // TODO: Write tests for this
+    public static CurrencyAmount parseAmount(String s) {
+        return new CurrencyAmount(-1L, Currency.getInstance("XCD"));
+    }
+
+    // TODO: Override equals(), hashCode(), toString()
+
+    // TODO: Make Comparable<CurrencyAmount>, override compareTo()
+
+    // TODO: Write tests for this
+    public CurrencyAmount(long centsAmount, Currency currency) {
+        //
+    }
+
 }
