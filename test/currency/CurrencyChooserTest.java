754f8e0d3b34afbe3d02f301a96469942072e59e
Alonso del Arte <alonso.delarte@gmail.com>
2025-01-01 21:58:23 -0500

    Failing chooseCurrency( ) test

--- test/currency/CurrencyChooserTest.java
+++ test/currency/CurrencyChooserTest.java
@@ -136,4 +136,28 @@ class CurrencyChooserTest {
         assertEquals(PSEUDO_CURRENCIES, actual);
     }
 
+    @Test
+    public void testChooseCurrency() {
+        System.out.println("chooseCurrency");
+        int totalNumberOfCurrencies = CURRENCIES.size();
+        int numberOfTries = 5 * totalNumberOfCurrencies / 3;
+        Set<Currency> samples = new HashSet<>();
+        int sampleNumber = 0;
+        while (sampleNumber < numberOfTries) {
+            Currency sample = CurrencyChooser.chooseCurrency();
+            String msg = "Chosen currency " + sample.getDisplayName()
+                    + " expected to not have negative fraction digits";
+            assert sample.getDefaultFractionDigits() > -1 : msg;
+            samples.add(sample);
+            sampleNumber++;
+        }
+        int expected = 11 * totalNumberOfCurrencies / 20;
+        int actual = samples.size();
+        String msg = "Trying to pick " + numberOfTries + " times from set of "
+                + totalNumberOfCurrencies + " gave " + actual
+                + " distinct, should've given more than " + expected
+                + " distinct";
+        assert expected <= actual : msg;
+    }
+
 }
