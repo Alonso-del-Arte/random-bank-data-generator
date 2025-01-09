6e7ecbaeedd2a355ec53a3fb3f257e7dca28a305
Alonso del Arte <alonso.delarte@gmail.com>
2025-01-08 19:56:58 -0500

    Passing test that choose currency by fraction digits excludes historical currencies

--- src/currency/CurrencyChooser.java
+++ src/currency/CurrencyChooser.java
@@ -131,8 +131,7 @@ public class CurrencyChooser {
                     = new ArrayList<>(CURRENCIES_DIGITS_MAP
                     .get(fractionDigits));
             int index = RANDOM.nextInt(currencies.size());
-            return Currency.getInstance("BYR");
-//            return currencies.get(index);
+            return currencies.get(index);
         } else {
             String excMsg = "No available currency has " + fractionDigits
                     + " fraction digits";
