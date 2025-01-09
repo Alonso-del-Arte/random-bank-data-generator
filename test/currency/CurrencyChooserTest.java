45702ab823ad81d47aa8153db5b5f29a9bd50868
Alonso del Arte <alonso.delarte@gmail.com>
2025-01-09 16:08:12 -0500

    Failing exclude São Tomé & Príncipe dobra (STN) test

--- test/currency/CurrencyChooserTest.java
+++ test/currency/CurrencyChooserTest.java
@@ -43,7 +43,8 @@ class CurrencyChooserTest {
             "ITL", "LUF", "MTL", "NLG", "PTE", "SIT"};
 
     private static final String[] OTHER_EXCLUSION_CODES = {"AYM", "BGL", "BOV",
-            "CHE", "CHW", "COU", "GWP", "MGF", "MXV", "SRG", "USN", "USS"};
+            "CHE", "CHW", "COU", "GWP", "MGF", "MXV", "SRG", "STN", "USN",
+            "USS"};
 
     static {
         for (Currency currency : CURRENCIES) {
@@ -570,6 +571,19 @@ class CurrencyChooserTest {
             String msg = "Currency " + currency.getDisplayName()
                     + " should not be " + surinameseGuilderDisplayName;
             assertNotEquals(surinameseGuilder, currency, msg);
+        }
+    }
+
+    @Test
+    public void testSaoTomeAndPrincipeDobraExcluded() {
+        Currency saoTomeAndPrincipeDobra = Currency.getInstance("STN");
+        String saoTomeAndPrincipeDobraDisplayName
+                = saoTomeAndPrincipeDobra.getDisplayName();
+        for (int i = 0; i < NUMBER_OF_CALLS_FOR_EXCLUSION_SEARCH; i++) {
+            Currency currency = CurrencyChooser.chooseCurrency();
+            String msg = "Currency " + currency.getDisplayName()
+                    + " should not be " + saoTomeAndPrincipeDobraDisplayName;
+            assertNotEquals(saoTomeAndPrincipeDobra, currency, msg);
         }
     }
 
