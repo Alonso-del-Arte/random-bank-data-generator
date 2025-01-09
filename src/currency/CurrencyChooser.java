f0b5855a8e6cb594ce38ea54ad480a727dc96464
Alonso del Arte <alonso.delarte@gmail.com>
2025-01-09 16:06:03 -0500

    Passing exclude Surinamese guilder (SRG) test

--- src/currency/CurrencyChooser.java
+++ src/currency/CurrencyChooser.java
@@ -36,7 +36,7 @@ public class CurrencyChooser {
     private static final String[] OTHER_EXCLUSION_CODES = {"ADP", "ATS", "AYM",
             "BEF", "BGL", "BOV", "CHE", "CHW", "COU", "CYP", "DEM", "EEK",
             "ESP", "FIM", "FRF", "GRD", "GWP", "IEP", "ITL", "LUF", "MGF",
-            "MTL", "MXV", "NLG", "PTE", "SIT", "USN", "USS"};
+            "MTL", "MXV", "NLG", "PTE", "SIT", "SRG", "USN", "USS"};
 
     private static final Map<Integer, Set<Currency>> CURRENCIES_DIGITS_MAP
             = new HashMap<>();
