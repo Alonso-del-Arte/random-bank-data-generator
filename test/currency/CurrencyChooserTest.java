c7782d55657538119da51b9d45defe5c0f454ce7
Alonso del Arte <alonso.delarte@gmail.com>
2025-01-08 22:53:57 -0500

    Failing Malagasy franc (MGF) excluded test

--- test/currency/CurrencyChooserTest.java
+++ test/currency/CurrencyChooserTest.java
@@ -40,7 +40,7 @@ class CurrencyChooserTest {
 
     private static final String[] EURO_REPLACED_EXCLUSION_CODES = {"ADP", "ATS",
             "BEF", "COU", "CYP", "DEM", "EEK", "ESP", "FIM", "FRF", "GRD",
-            "GWP", "IEP", "ITL", "LUF", "MTL", "NLG", "PTE", "SIT"};
+            "GWP", "IEP", "ITL", "LUF", "MGF", "MTL", "NLG", "PTE", "SIT"};
 
     private static final String[] OTHER_EXCLUSION_CODES = {"AYM", "BGL", "BOV",
             "CHE", "CHW", "USN", "USS"};
@@ -526,6 +526,18 @@ class CurrencyChooserTest {
             String msg = "Currency " + currency.getDisplayName()
                     + " should not be " + pesosDisplayName;
             assertNotEquals(pesos, currency, msg);
+        }
+    }
+
+    @Test
+    public void testMalagasyFrancExcluded() {
+        Currency francs = Currency.getInstance("MGF");
+        String francsDisplayName = francs.getDisplayName();
+        for (int i = 0; i < NUMBER_OF_CALLS_FOR_EXCLUSION_SEARCH; i++) {
+            Currency currency = CurrencyChooser.chooseCurrency();
+            String msg = "Currency " + currency.getDisplayName()
+                    + " should not be " + francsDisplayName;
+            assertNotEquals(francs, currency, msg);
         }
     }
 
