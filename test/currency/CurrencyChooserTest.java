5a5c30b8875e5e3c949588b715711892b8e318fd
Alonso del Arte <alonso.delarte@gmail.com>
2025-01-08 20:32:42 -0500

    Failing Bulgarian hard lev excluded test

--- test/currency/CurrencyChooserTest.java
+++ test/currency/CurrencyChooserTest.java
@@ -42,7 +42,8 @@ class CurrencyChooserTest {
             "BEF", "CYP", "DEM", "EEK", "ESP", "FIM", "FRF", "GRD", "IEP", "ITL",
             "LUF", "MTL", "NLG", "PTE", "SIT" */};
 
-    private static final String[] OTHER_EXCLUSION_CODES = {"AYM", "USN", "USS"};
+    private static final String[] OTHER_EXCLUSION_CODES = {"AYM", "BGL", "USN",
+            "USS"};
 
     static {
         for (Currency currency : CURRENCIES) {
@@ -453,6 +454,18 @@ class CurrencyChooserTest {
             String msg = "Currency " + currency.getDisplayName()
                     + " should not be " + improperManatDisplayName;
             assertNotEquals(improperManat, currency, msg);
+        }
+    }
+
+    @Test
+    public void testBulgarianHardLevExcluded() {
+        Currency bulgarianHardLev = Currency.getInstance("BGL");
+        String bulgarianHardLevDisplayName = bulgarianHardLev.getDisplayName();
+        for (int i = 0; i < NUMBER_OF_CALLS_FOR_EXCLUSION_SEARCH; i++) {
+            Currency currency = CurrencyChooser.chooseCurrency();
+            String msg = "Currency " + currency.getDisplayName()
+                    + " should not be " + bulgarianHardLevDisplayName;
+            assertNotEquals(bulgarianHardLev, currency, msg);
         }
     }
 
