6340a26e6014c7e3a2fdc52214853165b631dff5
Alonso del Arte <alonso.delarte@gmail.com>
2025-01-08 20:35:22 -0500

    Passing Bulgarian hard lev excluded test

--- src/currency/CurrencyChooser.java
+++ src/currency/CurrencyChooser.java
@@ -35,7 +35,8 @@ public class CurrencyChooser {
 
     private static final Set<Currency> OTHER_EXCLUSIONS = new HashSet<>();
 
-    private static final String[] OTHER_EXCLUSION_CODES = {"AYM", "USN", "USS"};
+    private static final String[] OTHER_EXCLUSION_CODES = {"AYM", "BGL", "USN",
+            "USS"};
 
     private static final Map<Integer, Set<Currency>> CURRENCIES_DIGITS_MAP
             = new HashMap<>();
