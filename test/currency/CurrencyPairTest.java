44e3ccff6be4ced30c46f1a7bd35e1d2f767b85b
Alonso del Arte <alonso.delarte@gmail.com>
2025-01-15 23:45:09 -0500

    Failing not equals null test

--- test/currency/CurrencyPairTest.java
+++ test/currency/CurrencyPairTest.java
@@ -50,6 +50,20 @@ class CurrencyPairTest {
         assert somePair.equals(somePair) : "Instance should be equal to itself";
     }
 
+    private static Object provideNull() {
+        return null;
+    }
+
+    @Test
+    public void testNotEqualsNull() {
+        Currency from = CurrencyChooser.chooseCurrency();
+        Currency to = CurrencyChooser.chooseCurrencyOtherThan(from);
+        CurrencyPair somePair = new CurrencyPair(from, to);
+        Object obj = provideNull();
+        String msg = "Pair " + somePair.toString() + " should not equal null";
+        assert !somePair.equals(obj) : msg;
+    }
+
     @Test
     public void testFlip() {
         System.out.println("flip");
