fb12b865977ae7a9c9691b6942b718fdf259f2e4
Alonso del Arte <alonso.delarte@gmail.com>
2025-01-21 00:38:49 -0500

    Failing constructor rejects null currency test

--- test/currency/CurrencyAmountTest.java
+++ test/currency/CurrencyAmountTest.java
@@ -9,4 +9,21 @@ public class CurrencyAmountTest {
 
     public static final Random RANDOM = new Random();
 
+    @Test
+    void testConstructorRejectsNullCurrency() {
+        int centsAmount = RANDOM.nextInt();
+        String message = "Trying to instantiate " + centsAmount
+                + " of null currency should cause an exception";
+        Throwable t = assertThrows(NullPointerException.class, () -> {
+            CurrencyAmount badAmount = new CurrencyAmount(centsAmount, null);
+            System.out.println(message + ", not created instance "
+                    + badAmount.getClass().getName() + '@'
+                    + Integer.toHexString(badAmount.hashCode()));
+        }, message);
+        String excMsg = t.getMessage();
+        assert excMsg != null : "Exception message should not be null";
+        assert !excMsg.isBlank() : "Exception message should not be blank";
+        System.out.println("\"" + excMsg + "\"");
+    }
+
 }
