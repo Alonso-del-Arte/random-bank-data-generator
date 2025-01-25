10bb318446ac65984892b0c5c4d82c1ae1d08709
Alonso del Arte <alonso.delarte@gmail.com>
2025-01-24 21:33:08 -0500

    Failing toString( ) for units plus 0 to 9 cents of other currency

--- test/currency/CurrencyAmountTest.java
+++ test/currency/CurrencyAmountTest.java
@@ -133,6 +133,24 @@ public class CurrencyAmountTest {
     }
 
     @Test
+    void testToStringUnitsPlusZeroToNineCentsOfOtherCurrency() {
+        Currency currency = CurrencyChooser.chooseCurrency(CRITERION_A);
+        String symbol = currency.getSymbol();
+        int unitsQty = RANDOM.nextInt(1000) + 1;
+        int unitsInCents = unitsQty * 100;
+        String partial = symbol + unitsQty + ".0";
+        for (short cents = 0; cents < 10; cents++) {
+            CurrencyAmount amount = new CurrencyAmount(unitsInCents + cents, currency);
+            String expected = partial + cents;
+            String actual = amount.toString();
+            String message = "toString() for " + unitsQty + " units and "
+                    + cents + " cents of " + currency.getDisplayName() + " ("
+                    + currency.getCurrencyCode() + ')';
+            assertEquals(expected, actual, message);
+        }
+    }
+
+    @Test
     void testConstructorRejectsNullCurrency() {
         int centsAmount = RANDOM.nextInt();
         String message = "Trying to instantiate " + centsAmount
