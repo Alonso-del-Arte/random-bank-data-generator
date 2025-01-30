2df623607d507f8dc30eab33daff8547e5831466
Alonso del Arte <alonso.delarte@gmail.com>
2025-01-30 18:07:01 -0500

    Failing constructor rejects excessive number test

--- test/entities/idnumbers/TaxpayerIdentificationNumberTest.java
+++ test/entities/idnumbers/TaxpayerIdentificationNumberTest.java
@@ -36,6 +36,26 @@ class TaxpayerIdentificationNumberTest {
         System.out.println("\"" + excMsg + "\"");
     }
 
+    @Test
+    void testConstructorRejectsExcessiveNumber() {
+        int badNum = TaxpayerIdentificationNumber.UPPER_NUMBER_LIMIT
+                + RANDOM.nextInt(Short.MAX_VALUE);
+        String message = "Constructor should reject number " + badNum;
+        Throwable t = assertThrows(IllegalArgumentException.class, () -> {
+            TaxpayerIdentificationNumber badInstance
+                    = new TaxpayerIdentificationNumberImpl(badNum);
+            System.out.println(message + ", not created instance "
+                    + badInstance);
+        }, message);
+        String excMsg = t.getMessage();
+        assert excMsg != null : "Message should not be null";
+        assert !excMsg.isBlank() : "Message should not be blank";
+        String numStr = Integer.toString(badNum);
+        String containsMsg = "Message should contain rejected number " + numStr;
+        assert excMsg.contains(numStr) : containsMsg;
+        System.out.println("\"" + excMsg + "\"");
+    }
+
     private static class TaxpayerIdentificationNumberImpl
             extends TaxpayerIdentificationNumber {
 
