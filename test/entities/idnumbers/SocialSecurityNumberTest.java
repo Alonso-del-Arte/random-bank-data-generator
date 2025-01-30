41259bc193514bb88b2888f28b46a2f4d0c4b90a
Alonso del Arte <alonso.delarte@gmail.com>
2025-01-29 21:15:51 -0500

    Failing constructor rejects excessive number test

--- test/entities/idnumbers/SocialSecurityNumberTest.java
+++ test/entities/idnumbers/SocialSecurityNumberTest.java
@@ -34,4 +34,23 @@ class SocialSecurityNumberTest {
         System.out.println("\"" + excMsg + "\"");
     }
 
+    @Test
+    void testConstructorRejectsNegativeNumber() {
+        int badNum = SocialSecurityNumber.UPPER_NUMBER_LIMIT
+                + RANDOM.nextInt(Short.MAX_VALUE);
+        String message = "Constructor should reject number " + badNum;
+        Throwable t = assertThrows(IllegalArgumentException.class, () -> {
+            SocialSecurityNumber badInstance = new SocialSecurityNumber(badNum);
+            System.out.println(message + ", not created instance "
+                    + badInstance);
+        }, message);
+        String excMsg = t.getMessage();
+        assert excMsg != null : "Message should not be null";
+        assert !excMsg.isBlank() : "Message should not be blank";
+        String numStr = Integer.toString(badNum);
+        String containsMsg = "Message should contain rejected number " + numStr;
+        assert excMsg.contains(numStr) : containsMsg;
+        System.out.println("\"" + excMsg + "\"");
+    }
+
 }
