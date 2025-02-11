1a33d8115fab9c2cad49582cfa828cc8998faec6
Alonso del Arte <alonso.delarte@gmail.com>
2025-02-11 16:58:43 -0500

    Getter stubs

--- src/currency/CurrencyConversionNeededException.java
+++ src/currency/CurrencyConversionNeededException.java
@@ -9,6 +9,32 @@ public class CurrencyConversionNeededException extends RuntimeException {
     private static final long serialVersionUID = 70580819529728L;
 
     // TODO: Write tests for this
+    @Override
+    public String getMessage() {
+        return "SORRY, NOT IMPLEMENTED YET";
+    }
+
+    // TODO: Write tests for this
+    public CurrencyAmount getAmountA() {
+        return new CurrencyAmount(-1, Currency.getInstance("USD"));
+    }
+
+    // TODO: Write tests for this
+    public CurrencyAmount getAmountB() {
+        return new CurrencyAmount(-1, Currency.getInstance("USD"));
+    }
+
+    // TODO: Write tests for this
+    public Currency getCurrencyA() {
+        return Currency.getInstance("XTS");
+    }
+
+    // TODO: Write tests for this
+    public Currency getCurrencyB() {
+        return Currency.getInstance("XTS");
+    }
+
+    // TODO: Write tests for this
     public CurrencyConversionNeededException(CurrencyAmount amount,
                                              Currency currency) {
         //
