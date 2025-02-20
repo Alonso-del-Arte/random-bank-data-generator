71ae9d0ab20a7599e1c082e47768d2d2e9a7fec9
Alonso del Arte <alonso.delarte@gmail.com>
2025-02-20 17:00:24 -0500

    Failing getMessage( ) test

--- test/currency/CurrencyConversionNeededExceptionTest.java
+++ test/currency/CurrencyConversionNeededExceptionTest.java
@@ -12,4 +12,31 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class CurrencyConversionNeededExceptionTest {
 
+    private static final String DEFAULT_MESSAGE = "FOR TESTING PURPOSES ONLY";
+
+    private static CurrencyAmount makeAmount() {
+        int centsAmount = RANDOM.nextInt();
+        Currency currency = CurrencyChooser.chooseCurrency();
+        return new CurrencyAmount(centsAmount, currency);
+    }
+
+    private static CurrencyAmount makeAmountDiffCurrency(Currency currency) {
+        int centsAmount = RANDOM.nextInt();
+        Currency other = CurrencyChooser.chooseCurrencyOtherThan(currency);
+        return new CurrencyAmount(centsAmount, other);
+    }
+
+    @Test
+    void testGetMessage() {
+        System.out.println("getMessage");
+        String expected = DEFAULT_MESSAGE + " " + RANDOM.nextInt();
+        CurrencyAmount amountA = makeAmount();
+        CurrencyAmount amountB = makeAmountDiffCurrency(amountA.getCurrency());
+        CurrencyConversionNeededException instance
+                = new CurrencyConversionNeededException(amountA, amountB,
+                expected);
+        String actual = instance.getMessage();
+        assertEquals(expected, actual);
+    }
+
 }
