c3879e91f553a7ae7175598df453df75b1ba2ca8
Alonso del Arte <alonso.delarte@gmail.com>
2025-02-20 16:22:52 -0500

    New test class

---
+++ test/currency/CurrencyConversionNeededExceptionTest.java
@@ -0,0 +1,15 @@
+package currency;
+
+import static currency.CurrencyAmountTest.RANDOM;
+
+import java.util.Currency;
+import java.util.Locale;
+import java.util.Random;
+import java.util.function.Predicate;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class CurrencyConversionNeededExceptionTest {
+
+}
