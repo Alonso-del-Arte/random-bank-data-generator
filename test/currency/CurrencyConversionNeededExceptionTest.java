6eb8321e6116590018f0d30f254b0b17804c220e
Alonso del Arte <alonso.delarte@gmail.com>
2025-02-20 23:20:41 -0500

    Failing primary constructor rejects null amount B test

--- test/currency/CurrencyConversionNeededExceptionTest.java
+++ test/currency/CurrencyConversionNeededExceptionTest.java
@@ -115,4 +115,22 @@ class CurrencyConversionNeededExceptionTest {
         System.out.println("\"" + excMsg + "\"");
     }
 
+    @Test
+    void testPrimaryConstructorRejectsNullAmountB() {
+        CurrencyAmount amountA = makeAmount();
+        String message = "Null amountA should cause exception";
+        Throwable t = assertThrows(NullPointerException.class, () -> {
+            CurrencyConversionNeededException instance
+                    = new CurrencyConversionNeededException(amountA, null,
+                    DEFAULT_MESSAGE);
+            System.out.println(message + ", not created instance "
+                    + instance.getClass().getName() + '@'
+                    + Integer.toHexString(instance.hashCode()));
+        }, message);
+        String excMsg = t.getMessage();
+        assert excMsg != null : "Exception message should not be null";
+        assert !excMsg.isBlank() : "Exception message should not be blank";
+        System.out.println("\"" + excMsg + "\"");
+    }
+
 }
