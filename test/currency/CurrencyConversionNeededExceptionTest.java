e3a8c9611f0b0d967168ecfe0e5225333e7b9bbb
Alonso del Arte <alonso.delarte@gmail.com>
2025-02-20 23:33:21 -0500

    Failing first aux constructor rejects null amount B test

--- test/currency/CurrencyConversionNeededExceptionTest.java
+++ test/currency/CurrencyConversionNeededExceptionTest.java
@@ -147,4 +147,22 @@ class CurrencyConversionNeededExceptionTest {
         System.out.println("\"" + excMsg + "\"");
     }
 
+    @Test
+    void testFirstAuxConstructorRejectsNullAmountB() {
+        CurrencyAmount amountA = makeAmount();
+        String message = "Null amountB should cause exception";
+        Throwable t = assertThrows(NullPointerException.class, () -> {
+            CurrencyConversionNeededException instance
+                    = new CurrencyConversionNeededException(amountA,
+                    (CurrencyAmount) null);
+            System.out.println(message + ", not created instance "
+                    + instance.getClass().getName() + '@'
+                    + Integer.toHexString(instance.hashCode()));
+        }, message);
+        String excMsg = t.getMessage();
+        assert excMsg != null : "Exception message should not be null";
+        assert !excMsg.isBlank() : "Exception message should not be blank";
+        System.out.println("\"" + excMsg + "\"");
+    }
+
 }
