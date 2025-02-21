45cf7ceed0d615ea1c4a1a73abedd0d603e38b54
Alonso del Arte <alonso.delarte@gmail.com>
2025-02-20 23:35:18 -0500

    Passing first aux constructor rejects null amount B test

--- src/currency/CurrencyConversionNeededException.java
+++ src/currency/CurrencyConversionNeededException.java
@@ -48,6 +48,10 @@ public class CurrencyConversionNeededException extends RuntimeException {
             String excMsg = "Amount A should not be null";
             throw new NullPointerException(excMsg);
         }
+        if (amountB == null) {
+            String excMsg = "Amount B should not be null";
+            throw new NullPointerException(excMsg);
+        }
         this.amtA = new CurrencyAmount(-1, Currency.getInstance("USD"));
         this.amtB = this.amtA;
     }
