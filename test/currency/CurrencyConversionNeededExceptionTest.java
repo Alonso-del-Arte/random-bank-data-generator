656bfae3f9c95b3b67824b03ee943a0598ed6757
Alonso del Arte <alonso.delarte@gmail.com>
2025-02-24 20:35:40 -0500

    Failing test for amountB not specified, currency B having 100 divisions

--- test/currency/CurrencyConversionNeededExceptionTest.java
+++ test/currency/CurrencyConversionNeededExceptionTest.java
@@ -128,8 +128,23 @@ class CurrencyConversionNeededExceptionTest {
         assertEquals(expected, actual, message);
     }
 
-    // TODO: Write tests for getAmountB() on instances constructed from
-    //  second auxiliary constructor
+    @Test
+    void testGetAmountBOnInstanceFromSecondAuxConstructor100Cents() {
+        CurrencyAmount amountA = makeAmount();
+        Currency currA = amountA.getCurrency();
+        Currency currency = CurrencyChooser.chooseCurrency(
+                (cur) -> !cur.equals(currA)
+                        && cur.getDefaultFractionDigits() == 2
+        );
+        CurrencyConversionNeededException instance
+                = new CurrencyConversionNeededException(amountA, currency);
+        CurrencyAmount expected = new CurrencyAmount(100, currency);
+        CurrencyAmount actual = instance.getAmountB();
+        String message = "As amount of currency " + currency.getDisplayName()
+                + " (" + currency.getCurrencyCode()
+                + ") was not specified, expecting one unit of that currency";
+        assertEquals(expected, actual, message);
+    }
 
     @Test
     void testGetCurrencyA() {
