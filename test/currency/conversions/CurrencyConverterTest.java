1c004533b3154c3ad4e8cdbc319a7ebd5a3fa2d4
Alonso del Arte <alonso.delarte@gmail.com>
2025-02-27 00:35:42 -0500

    New test class

---
+++ test/currency/conversions/CurrencyConverterTest.java
@@ -0,0 +1,22 @@
+package currency.conversions;
+
+import currency.CurrencyAmount;
+import currency.CurrencyChooser;
+import currency.CurrencyPair;
+
+import java.util.Currency;
+import java.util.Locale;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class CurrencyConverterTest {
+
+    private static final double TEST_DELTA = 0.001;
+
+    private static final Currency U_S_DOLLARS = Currency.getInstance(Locale.US);
+
+    private static final Currency EAST_CARIBBEAN_DOLLARS
+            = Currency.getInstance("XCD");
+
+}
