4a05c1649c6bde5352ed9d322302d06404e4a0e0
Alonso del Arte <alonso.delarte@gmail.com>
2025-02-26 23:53:32 -0500

    Changed to interface

--- src/currency/conversions/ExchangeRateProvider.java
+++ src/currency/conversions/ExchangeRateProvider.java
@@ -1,4 +1,27 @@
 package currency.conversions;
 
-public class ExchangeRateProvider {
+import currency.CurrencyPair;
+
+import java.util.Currency;
+
+public interface ExchangeRateProvider {
+
+    /**
+     * Gives the rate to convert one unit of the source currency to the target
+     * currency.
+     * @param source The source to convert from. For example, United States
+     * dollars (USD).
+     * @param target The target to convert one unit of {@code source} to. For
+     * example, euros (EUR).
+     * @return The conversion rate. In the example as of August 12, 2024, this
+     * was 0.915796.
+     * @throws RuntimeException If some kind of {@code IOException} or other
+     * checked exception occurs, it will be wrapped into an unchecked exception.
+     */
+    double getRate(Currency source, Currency target);
+
+    default double getRate(CurrencyPair currencies) {
+        return -0.0;
+    }
+
 }
