4a27a10da611ce1267db76c13300963ec95ad2ca
Alonso del Arte <alonso.delarte@gmail.com>
2025-02-26 23:49:38 -0500

    New package

---
+++ src/currency/conversions/ConversionRateQuote.java
@@ -0,0 +1,4 @@
+package currency.conversions;
+
+public class ConversionRateQuote {
+}
---
+++ test/currency/conversions/RateQuoteCacheTest.java
@@ -0,0 +1,7 @@
+package currency.conversions;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RateQuoteCacheTest {
+
+}
