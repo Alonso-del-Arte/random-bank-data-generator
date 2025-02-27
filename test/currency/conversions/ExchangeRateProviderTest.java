0206113c5e0ab698ca33e764ea37dab6f2869c50
Alonso del Arte <alonso.delarte@gmail.com>
2025-02-26 23:56:07 -0500

    Preparing to write tests

--- test/currency/conversions/ExchangeRateProviderTest.java
+++ test/currency/conversions/ExchangeRateProviderTest.java
@@ -1,7 +1,38 @@
 package currency.conversions;
 
+import currency.CurrencyChooser;
+import currency.CurrencyPair;
+
+import java.util.Currency;
+import java.util.Random;
+
 import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
 
 class ExchangeRateProviderTest {
+
+    private static final Random RANDOM = new Random();
+
+    private static class ExchangeRateProviderImpl
+            implements ExchangeRateProvider {
+
+        private int nonDefaultGetRateCallCount = 0;
+
+        private Currency mostRecentSource;
+
+        private Currency mostRecentTarget;
+
+        private double mostRecentReturn = Double.NaN;
+
+        @Override
+        public double getRate(Currency source, Currency target) {
+            this.nonDefaultGetRateCallCount++;
+            this.mostRecentSource = source;
+            this.mostRecentTarget = target;
+            this.mostRecentReturn = 1.0 + RANDOM.nextDouble();
+            return this.mostRecentReturn;
+        }
+
+    }
 
 }
