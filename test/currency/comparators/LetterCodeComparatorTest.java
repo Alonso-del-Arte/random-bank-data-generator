e9e559d99c67cdce8680969b5a2e3757410b7215
Alonso del Arte <alonso.delarte@gmail.com>
2025-02-26 20:43:07 -0500

    Removed unused import

--- test/currency/comparators/LetterCodeComparatorTest.java
+++ test/currency/comparators/LetterCodeComparatorTest.java
@@ -2,7 +2,6 @@ package currency.comparators;
 
 import java.util.Arrays;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Currency;
 import java.util.List;
 import java.util.Set;
