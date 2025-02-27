38c0a7e876a1120707d5915b6ecb8bf2c4ad693d
Alonso del Arte <alonso.delarte@gmail.com>
2025-02-26 20:35:01 -0500

    Failing compare( ) test

--- test/currency/comparators/LetterCodeComparatorTest.java
+++ test/currency/comparators/LetterCodeComparatorTest.java
@@ -1,7 +1,35 @@
 package currency.comparators;
 
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Currency;
+import java.util.List;
+import java.util.Set;
+
 import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
 
 class LetterCodeComparatorTest {
+
+    private static final Set<Currency> ALL_CURRENCIES
+            = Currency.getAvailableCurrencies();
+
+    @Test
+    public void testCompare() {
+        int initialCapacity = ALL_CURRENCIES.size();
+        List<Currency> expected = new ArrayList<>(initialCapacity);
+        List<Currency> actual = new ArrayList<>(ALL_CURRENCIES);
+        String[] letterCodes = new String[initialCapacity];
+        for (int i = 0; i < initialCapacity; i++) {
+            letterCodes[i] = actual.get(i).getCurrencyCode();
+        }
+        Arrays.sort(letterCodes);
+        for (int j = 0; j < initialCapacity; j++) {
+            expected.add(Currency.getInstance(letterCodes[j]));
+        }
+        actual.sort(new LetterCodeComparator());
+        assertEquals(expected, actual);
+    }
 
 }
