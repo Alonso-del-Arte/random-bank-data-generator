0aa9645cb67a826b24faceb52a3269b9329d5eca
Alonso del Arte <alonso.delarte@gmail.com>
2025-02-27 00:22:28 -0500

    Function, constructor stubs

--- src/currency/conversions/CurrencyConverter.java
+++ src/currency/conversions/CurrencyConverter.java
@@ -1,4 +1,24 @@
 package currency.conversions;
 
+import currency.CurrencyAmount;
+
+import java.util.Currency;
+
 public class CurrencyConverter {
+
+    // TODO: Write tests for this
+    public ExchangeRateProvider getProvider() {
+        return null;
+    }
+
+    // TODO: Write tests for this
+    public CurrencyAmount convert(CurrencyAmount source, Currency target) {
+        return source;
+    }
+
+    // TODO: Write tests for this
+    public CurrencyConverter(ExchangeRateProvider rateProvider) {
+        //
+    }
+
 }
