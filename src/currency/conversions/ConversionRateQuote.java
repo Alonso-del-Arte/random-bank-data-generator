1f07a811db080c0f1c55ea59303ed2e35fdd7219
Alonso del Arte <alonso.delarte@gmail.com>
2025-03-05 13:35:39 -0500

    Function, constructor stubs

--- src/currency/conversions/ConversionRateQuote.java
+++ src/currency/conversions/ConversionRateQuote.java
@@ -1,4 +1,41 @@
 package currency.conversions;
 
+import currency.CurrencyPair;
+
+import java.time.LocalDateTime;
+
 public class ConversionRateQuote {
+
+    // TODO: Write tests for this
+    public CurrencyPair getCurrencies() {
+        return new CurrencyPair(java.util.Currency.getInstance("XTS"),
+                java.util.Currency.getInstance("XTS"));
+    }
+
+    // TODO: Write tests for this
+    public double getRate() {
+        return -1.0;
+    }
+
+    // TODO: Write tests for this
+    public LocalDateTime getDate() {
+        return LocalDateTime.now().minusYears(100);
+    }
+
+    // TODO: Write tests for this
+    public ConversionRateQuote invert() {
+        return this;
+    }
+
+    // TODO: Write tests for this
+    public ConversionRateQuote(CurrencyPair currencies, double rate) {
+        //
+    }
+
+    // TODO: Write tests for this
+    public ConversionRateQuote(CurrencyPair currencies, double rate,
+                               LocalDateTime date) {
+        //
+    }
+
 }
