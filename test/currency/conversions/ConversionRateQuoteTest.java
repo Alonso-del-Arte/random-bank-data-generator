edd29cd58511f1febb2fd210d88c960693d45115
Alonso del Arte <alonso.delarte@gmail.com>
2025-03-05 14:01:05 -0500

    Failing getCurrencies( ) test

--- test/currency/conversions/ConversionRateQuoteTest.java
+++ test/currency/conversions/ConversionRateQuoteTest.java
@@ -2,6 +2,7 @@ package currency.conversions;
 
 import currency.CurrencyChooser;
 import currency.CurrencyPair;
+import static currency.conversions.ExchangeRateProviderTest.RANDOM;
 
 import java.time.LocalDateTime;
 import java.util.Currency;
@@ -12,5 +13,19 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 class ConversionRateQuoteTest {
+
+    @Test
+    public void testGetCurrencies() {
+        System.out.println("getCurrencies");
+        Currency from = CurrencyChooser.chooseCurrency();
+        Currency to = CurrencyChooser.chooseCurrencyOtherThan(from);
+        CurrencyPair expected = new CurrencyPair(from, to);
+        double rate = 0.5 + RANDOM.nextDouble();
+        LocalDateTime date = LocalDateTime.now();
+        ConversionRateQuote instance = new ConversionRateQuote(expected, rate,
+                date);
+        CurrencyPair actual = instance.getCurrencies();
+        assertEquals(expected, actual);
+    }
 
 }
