95976c6d3db722c658a214f08f94d01b7ae52210
Alonso del Arte <alonso.delarte@gmail.com>
2025-03-05 14:40:50 -0500

    Failing getDate( ) test

--- test/currency/conversions/ConversionRateQuoteTest.java
+++ test/currency/conversions/ConversionRateQuoteTest.java
@@ -5,6 +5,7 @@ import currency.CurrencyPair;
 import static currency.conversions.ExchangeRateProviderTest.RANDOM;
 
 import java.time.LocalDateTime;
+import java.time.Year;
 import java.util.Currency;
 import java.util.HashSet;
 import java.util.Set;
@@ -63,6 +64,23 @@ class ConversionRateQuoteTest {
         ConversionRateQuote instance = new ConversionRateQuote(currencies,
                 expected);
         double actual = instance.getRate();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testGetDate() {
+        System.out.println("getDate");
+        Currency from = CurrencyChooser.chooseCurrency();
+        Currency to = CurrencyChooser.chooseCurrencyOtherThan(from);
+        CurrencyPair currencies = new CurrencyPair(from, to);
+        double rate = 0.5 + RANDOM.nextDouble();
+        LocalDateTime now = LocalDateTime.now();
+        int days = RANDOM.nextInt(now.getMonth().length(Year
+                .isLeap(now.getYear())));
+        LocalDateTime expected = now.minusDays(days);
+        ConversionRateQuote instance = new ConversionRateQuote(currencies, rate,
+                expected);
+        LocalDateTime actual = instance.getDate();
         assertEquals(expected, actual);
     }
 
