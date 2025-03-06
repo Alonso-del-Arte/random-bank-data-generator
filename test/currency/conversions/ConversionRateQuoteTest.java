0c87118565a3628a7a3c09933a8a52bc2ea8cf40
Alonso del Arte <alonso.delarte@gmail.com>
2025-03-05 23:39:29 -0500

    Failing test that primary constructor rejects -Infinity for conversion rate

--- test/currency/conversions/ConversionRateQuoteTest.java
+++ test/currency/conversions/ConversionRateQuoteTest.java
@@ -137,4 +137,26 @@ class ConversionRateQuoteTest {
         System.out.println("\"" + excMsg + "\"");
     }
 
+    @Test
+    public void testConstructorRejectsNegativeInfinityRate() {
+        Currency from = CurrencyChooser.chooseCurrency();
+        Currency to = CurrencyChooser.chooseCurrencyOtherThan(from);
+        CurrencyPair currencies = new CurrencyPair(from, to);
+        double rate = Double.NEGATIVE_INFINITY;
+        LocalDateTime date = LocalDateTime.now();
+        String message = "Using " + rate
+                + " for rate should've caused exception";
+        Throwable t = assertThrows(IllegalArgumentException.class, () -> {
+            ConversionRateQuote instance = new ConversionRateQuote(currencies,
+                    rate, date);
+            System.out.println(message + ", not created instance "
+                    + instance.getClass().getName() + '@'
+                    + Integer.toHexString(System.identityHashCode(instance)));
+        }, message);
+        String excMsg = t.getMessage();
+        assert excMsg != null : "Exception message should not be null";
+        assert !excMsg.isBlank() : "Exception message should not be blank";
+        System.out.println("\"" + excMsg + "\"");
+    }
+
 }
