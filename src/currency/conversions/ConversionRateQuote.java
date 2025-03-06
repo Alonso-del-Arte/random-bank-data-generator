2b5c505347626df13fb2557f15d3b1aa891cf9ce
Alonso del Arte <alonso.delarte@gmail.com>
2025-03-05 23:45:19 -0500

    Passing test that aux constructor rejects -Infinity for conversion rate

--- src/currency/conversions/ConversionRateQuote.java
+++ src/currency/conversions/ConversionRateQuote.java
@@ -34,6 +34,10 @@ public class ConversionRateQuote {
         if (currencies == null) {
             throw new NullPointerException("Currencies should not be null");
         }
+        if (rate == Double.NEGATIVE_INFINITY) {
+            String excMsg = "Rate " + rate + " is not valid";
+            throw new IllegalArgumentException(excMsg);
+        }
         this.pair = currencies;
         this.conversionRate = rate;
         this.fetchDate = LocalDateTime.now();
