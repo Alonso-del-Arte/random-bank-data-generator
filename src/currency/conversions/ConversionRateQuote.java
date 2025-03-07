bf3ea1f04ca4ce21086f1956f80c8fc7c03c94a2
Alonso del Arte <alonso.delarte@gmail.com>
2025-03-07 14:56:12 -0500

    Refactored equals( ) by deleting it

--- src/currency/conversions/ConversionRateQuote.java
+++ src/currency/conversions/ConversionRateQuote.java
@@ -31,20 +31,6 @@ public class ConversionRateQuote {
     }
 
     @Override
-    public boolean equals(Object obj) {
-        if (this == obj) {
-            return true;
-        }
-        if (obj == null) {
-            return false;
-        }
-        if (!this.getClass().equals(obj.getClass())) {
-            return false;
-        }
-        return this.pair.equals(((ConversionRateQuote) obj).pair);
-    }
-
-    @Override
     public int hashCode() {
         return 0;
     }
