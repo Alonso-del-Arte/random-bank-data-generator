24817addd0a29b76c78f003783d2101a336a1cb2
Alonso del Arte <alonso.delarte@gmail.com>
2025-03-11 23:44:19 -0400

    New class

---
+++ src/currency/conversions/HardCodedRateProvider.java
@@ -0,0 +1,41 @@
+package currency.conversions;
+
+import currency.CurrencyPair;
+import currency.SpecificCurrenciesSupport;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.Currency;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import currency.CurrencyPair;
+import currency.SpecificCurrenciesSupport;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.Currency;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class HardCodedRateProvider implements ExchangeRateProvider,
+        SpecificCurrenciesSupport {
+
+    public static final LocalDate DATE_OF_HARD_CODING
+            = LocalDate.of(2025, Month.MARCH, 29);
+
+    // TODO: Write tests for this
+    @Override
+    public Set<Currency> supportedCurrencies() {
+        return new HashSet<>();
+    }
+
+    // TODO: Write tests for this
+    @Override
+    public double getRate(Currency source, Currency target) {
+        return -1.0;
+    }
+
+}
---
+++ test/currency/conversions/HardCodedRateProviderTest.java
@@ -0,0 +1,8 @@
+package currency.conversions;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+class HardCodedRateProviderTest {
+
+}
