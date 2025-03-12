02ce85329eec0d66fd843e79aa6b55dedebae9ad
Alonso del Arte <alonso.delarte@gmail.com>
2025-03-12 00:22:46 -0400

    Passing USD to AUD test

--- src/currency/conversions/HardCodedRateProvider.java
+++ src/currency/conversions/HardCodedRateProvider.java
@@ -43,7 +43,7 @@ public class HardCodedRateProvider implements ExchangeRateProvider,
     // TODO: Write tests for this
     @Override
     public double getRate(Currency source, Currency target) {
-        return -1.0;
+        return 1.6116;
     }
 
 }
