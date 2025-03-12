172cff2c56461802b0787d1da5b17f91f7e4b191
Alonso del Arte <alonso.delarte@gmail.com>
2025-03-11 23:46:43 -0400

    Failing date of hard-coding test

--- test/currency/conversions/HardCodedRateProviderTest.java
+++ test/currency/conversions/HardCodedRateProviderTest.java
@@ -1,8 +1,27 @@
 package currency.conversions;
 
+import currency.CurrencyChooser;
+import currency.CurrencyPair;
+import currency.SpecificCurrenciesSupport;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.Currency;
+import java.util.HashSet;
+import java.util.Locale;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 class HardCodedRateProviderTest {
+
+    @Test
+    public void testDateOfHardCodingConstant() {
+        LocalDate expected = LocalDate.of(2025, Month.MARCH, 3);
+        LocalDate actual = HardCodedRateProvider.DATE_OF_HARD_CODING;
+        assertEquals(actual, expected);
+    }
 
 }
