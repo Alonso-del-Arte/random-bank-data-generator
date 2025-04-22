b6c537803c9e27d9182b0dbd7e36a6b771335297
Alonso del Arte <alonso.delarte@gmail.com>
2025-04-21 22:38:03 -0400

    Failing negative amount is not positive test

--- test/currency/CurrencyAmountTest.java
+++ test/currency/CurrencyAmountTest.java
@@ -526,6 +526,17 @@ public class CurrencyAmountTest {
         assert !amount.isPositive() : msg;
     }
 
+    @Test
+    void testNegativePositiveIsNot() {
+        int centsAmount = -RANDOM.nextInt(1, 128000);
+        Currency currency = CurrencyChooser.chooseCurrency();
+        CurrencyAmount amount = new CurrencyAmount(centsAmount, currency);
+        String msg = "Amount " + amount + " (" + currency.getDisplayName()
+                + ", " + currency.getCurrencyCode()
+                + ") should not be positive";
+        assert !amount.isPositive() : msg;
+    }
+
     private static Object passThrough(Object obj) {
         return obj;
     }
