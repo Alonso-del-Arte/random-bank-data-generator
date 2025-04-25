4d0d41fab9fd241ed175ef2cfd6956fb9effff5e
Alonso del Arte <alonso.delarte@gmail.com>
2025-04-24 20:25:46 -0400

    Failing positive is zero but is not test

--- test/currency/CurrencyAmountTest.java
+++ test/currency/CurrencyAmountTest.java
@@ -579,6 +579,16 @@ public class CurrencyAmountTest {
         assert amount.isZero() : msg;
     }
 
+    @Test
+    void testPositiveIsZeroButIsNot() {
+        int centsAmount = RANDOM.nextInt(1, 128000);
+        Currency currency = CurrencyChooser.chooseCurrency();
+        CurrencyAmount amount = new CurrencyAmount(centsAmount, currency);
+        String msg = "Amount " + amount + " (" + currency.getDisplayName()
+                + ", " + currency.getCurrencyCode() + ") should not be zero";
+        assert !amount.isZero() : msg;
+    }
+
     private static Object passThrough(Object obj) {
         return obj;
     }
