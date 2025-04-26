b7b9ed8732f555aaf7513e5f8c860a4ee1060b4f
Alonso del Arte <alonso.delarte@gmail.com>
2025-04-25 22:52:59 -0400

    Added a Random for this package

--- test/accounts/transactions/TransactionTest.java
+++ test/accounts/transactions/TransactionTest.java
@@ -3,11 +3,14 @@ package accounts.transactions;
 import currency.CurrencyAmount;
 
 import java.time.LocalDateTime;
+import java.util.Random;
 
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 class TransactionTest {
+
+    static final Random RANDOM = new Random(~(System.currentTimeMillis() << 3));
 
     private static class TransactionImpl extends Transaction {
 
