62b4003f47f09d04b16264536c5f3c54896030fe
Alonso del Arte <alonso.delarte@gmail.com>
2025-04-25 23:37:52 -0400

    Failing constructor rejects null date test

--- test/accounts/transactions/DepositTest.java
+++ test/accounts/transactions/DepositTest.java
@@ -3,8 +3,10 @@ package accounts.transactions;
 import static accounts.transactions.TransactionTest.RANDOM;
 
 import currency.CurrencyAmount;
+import static currency.CurrencyChooser.chooseCurrency;
 
 import java.time.LocalDateTime;
+import java.util.Currency;
 
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
@@ -15,9 +17,29 @@ class DepositTest {
     void testConstructorRejectsNullAmount() {
         LocalDateTime date = LocalDateTime.now().minusHours(RANDOM.nextInt(24));
         String message = "Constructor should reject date " + date
-                + " with null deposit";
+                + " with null amount";
         Throwable t = assertThrows(NullPointerException.class, () -> {
             Deposit badInstance = new Deposit(null, date);
+            System.out.println(message + ", not created instance "
+                    + badInstance);
+        }, message);
+        String excMsg = t.getMessage();
+        assert excMsg != null : "Exception message should not be null";
+        assert !excMsg.isBlank() : "Exception message should not be blank";
+        System.out.println("\"" + excMsg + "\"");
+    }
+
+    @Test
+    void testConstructorRejectsNullDate() {
+        Currency currency = chooseCurrency();
+        int amountInSubunits = RANDOM.nextInt(10000);
+        CurrencyAmount amount = new CurrencyAmount(amountInSubunits, currency);
+        LocalDateTime date = LocalDateTime.now().minusHours(RANDOM.nextInt(24));
+        String message = "Constructor should reject amount " + amount + " of "
+                + currency.getDisplayName() + " (" + currency.getCurrencyCode()
+                + ") with null date";
+        Throwable t = assertThrows(NullPointerException.class, () -> {
+            Deposit badInstance = new Deposit(amount, null);
             System.out.println(message + ", not created instance "
                     + badInstance);
         }, message);
