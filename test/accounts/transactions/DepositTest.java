8eb1ae684e4ddbd005f2d39f180e14996f63391e
Alonso del Arte <alonso.delarte@gmail.com>
2025-04-25 22:59:53 -0400

    Failing constructor rejects null amount test

--- test/accounts/transactions/DepositTest.java
+++ test/accounts/transactions/DepositTest.java
@@ -1,5 +1,7 @@
 package accounts.transactions;
 
+import static accounts.transactions.TransactionTest.RANDOM;
+
 import currency.CurrencyAmount;
 
 import java.time.LocalDateTime;
@@ -8,5 +10,21 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 class DepositTest {
+
+    @Test
+    void testConstructorRejectsNullAmount() {
+        LocalDateTime date = LocalDateTime.now().minusHours(RANDOM.nextInt(24));
+        String message = "Constructor should reject date " + date
+                + " with null deposit";
+        Throwable t = assertThrows(NullPointerException.class, () -> {
+            Deposit badInstance = new Deposit(null, date);
+            System.out.println(message + ", not created instance "
+                    + badInstance);
+        }, message);
+        String excMsg = t.getMessage();
+        assert excMsg != null : "Exception message should not be null";
+        assert !excMsg.isBlank() : "Exception message should not be blank";
+        System.out.println("\"" + excMsg + "\"");
+    }
 
 }
