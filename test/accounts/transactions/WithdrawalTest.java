76fa14c4e09d2598191e28befe4db28c3fba5bb9
Alonso del Arte <alonso.delarte@gmail.com>
2025-05-05 11:47:40 -0400

    New test class

---
+++ test/accounts/transactions/WithdrawalTest.java
@@ -0,0 +1,31 @@
+package accounts.transactions;
+
+import static accounts.transactions.TransactionTest.RANDOM;
+import currency.CurrencyAmount;
+import static currency.CurrencyChooser.chooseCurrency;
+
+import java.time.LocalDateTime;
+import java.util.Currency;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+class WithdrawalTest {
+
+    @Test
+    void testConstructorRejectsNullAmount() {
+        LocalDateTime date = LocalDateTime.now().plusHours(RANDOM.nextInt(24));
+        String message = "Constructor should reject date " + date
+                + " with null amount";
+        Throwable t = assertThrows(NullPointerException.class, () -> {
+            Withdrawal badInstance = new Withdrawal(null, date);
+            System.out.println(message + ", not created instance "
+                    + badInstance);
+        }, message);
+        String excMsg = t.getMessage();
+        assert excMsg != null : "Exception message should not be null";
+        assert !excMsg.isBlank() : "Exception message should not be blank";
+        System.out.println("\"" + excMsg + "\"");
+    }
+
+}
