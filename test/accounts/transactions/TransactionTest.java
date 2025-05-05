6ca618812731c558991d879014ce28e1e66ac6a2
Alonso del Arte <alonso.delarte@gmail.com>
2025-05-05 11:53:36 -0400

    Failing constructor rejects null amount test

--- test/accounts/transactions/TransactionTest.java
+++ test/accounts/transactions/TransactionTest.java
@@ -12,6 +12,22 @@ class TransactionTest {
 
     static final Random RANDOM = new Random(~(System.currentTimeMillis() << 3));
 
+    @Test
+    void testConstructorRejectsNullAmount() {
+        LocalDateTime date = LocalDateTime.now().minusHours(RANDOM.nextInt(24));
+        String message = "Constructor should reject date " + date
+                + " with null amount";
+        Throwable t = assertThrows(NullPointerException.class, () -> {
+            Transaction badInstance = new TransactionImpl(null, date);
+            System.out.println(message + ", not created instance "
+                    + badInstance);
+        }, message);
+        String excMsg = t.getMessage();
+        assert excMsg != null : "Exception message should not be null";
+        assert !excMsg.isBlank() : "Exception message should not be blank";
+        System.out.println("\"" + excMsg + "\"");
+    }
+
     private static class TransactionImpl extends Transaction {
 
         TransactionImpl(CurrencyAmount amount, LocalDateTime date) {
