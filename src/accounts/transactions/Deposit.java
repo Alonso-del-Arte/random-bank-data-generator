bfdad9a2795248999eaabbdb49842f5d449f8c7a
Alonso del Arte <alonso.delarte@gmail.com>
2025-05-07 21:39:48 -0400

    Refactored constructor to rely on superclass constructor to reject null parameters

--- src/accounts/transactions/Deposit.java
+++ src/accounts/transactions/Deposit.java
@@ -8,14 +8,6 @@ public class Deposit extends Transaction {
 
     public Deposit(CurrencyAmount amount, LocalDateTime date) {
         super(amount, date);
-        if (amount == null) {
-            String excMsg = "Amount should not be null";
-            throw new NullPointerException(excMsg);
-        }
-        if (date == null) {
-            String excMsg = "Date should not be null";
-            throw new NullPointerException(excMsg);
-        }
     }
 
 }
--- src/accounts/transactions/Withdrawal.java
+++ src/accounts/transactions/Withdrawal.java
@@ -8,14 +8,6 @@ public class Withdrawal extends Transaction {
 
     public Withdrawal(CurrencyAmount amount, LocalDateTime date) {
         super(amount, date);
-        if (amount == null) {
-            String excMsg = "Amount should not be null";
-            throw new NullPointerException(excMsg);
-        }
-        if (date == null) {
-            String excMsg = "Date should not be null";
-            throw new NullPointerException(excMsg);
-        }
     }
 
 }
