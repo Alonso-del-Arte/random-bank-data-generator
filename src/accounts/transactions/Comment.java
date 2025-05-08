c3f92887446e24d6d563b22ba32df70e4d464ac7
Alonso del Arte <alonso.delarte@gmail.com>
2025-05-08 06:56:09 -0400

    Changed constructor parameters

--- src/accounts/transactions/Comment.java
+++ src/accounts/transactions/Comment.java
@@ -3,10 +3,11 @@ package accounts.transactions;
 import currency.CurrencyAmount;
 
 import java.time.LocalDateTime;
+import java.util.Currency;
 
 public class Comment /* extends Transaction */ {
 
-    public Comment(CurrencyAmount amount, LocalDateTime date) {
+    public Comment(String text, Currency currency, LocalDateTime date) {
 //        super(amount, date);
     }
 
