3b4a5cc2b96aa0c910a4ac3702eb7def80dc0bcd
Alonso del Arte <alonso.delarte@gmail.com>
2025-05-07 21:43:19 -0400

    New test class

---
+++ test/accounts/transactions/CommentTest.java
@@ -0,0 +1,15 @@
+package accounts.transactions;
+
+import static accounts.transactions.TransactionTest.RANDOM;
+import currency.CurrencyAmount;
+import static currency.CurrencyChooser.chooseCurrency;
+
+import java.time.LocalDateTime;
+import java.util.Currency;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+class CommentTest {
+
+}
