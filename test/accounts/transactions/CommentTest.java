714ba5cb1277cf74ede6ded0dba80cac0e2a3967
Alonso del Arte <alonso.delarte@gmail.com>
2025-05-08 23:58:29 -0400

    Failing constructor rejects null text test

--- test/accounts/transactions/CommentTest.java
+++ test/accounts/transactions/CommentTest.java
@@ -15,6 +15,23 @@ class CommentTest {
     private static final String DEFAULT_TEXT = "For testing purposes only";
 
     @Test
+    void testConstructorRejectsNullText() {
+        Currency currency = chooseCurrency();
+        LocalDateTime date = LocalDateTime.now().minusHours(RANDOM.nextInt(24));
+        String message = "Constructor should reject date " + date
+                + " with null text";
+        Throwable t = assertThrows(NullPointerException.class, () -> {
+            Comment badInstance = new Comment(null, currency, date);
+            System.out.println(message + ", not created instance "
+                    + badInstance);
+        }, message);
+        String excMsg = t.getMessage();
+        assert excMsg != null : "Exception message should not be null";
+        assert !excMsg.isBlank() : "Exception message should not be blank";
+        System.out.println("\"" + excMsg + "\"");
+    }
+
+    @Test
     void testConstructorRejectsNullCurrency() {
         LocalDateTime date = LocalDateTime.now().minusHours(RANDOM.nextInt(24));
         String message = "Constructor should reject date " + date
