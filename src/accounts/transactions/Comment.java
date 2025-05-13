650bd9c931e3599866e7ba32c87f7179121b9887
Alonso del Arte <alonso.delarte@gmail.com>
2025-05-13 15:49:12 -0400

    Refactored constructor

--- src/accounts/transactions/Comment.java
+++ src/accounts/transactions/Comment.java
@@ -37,10 +37,6 @@ public class Comment extends Transaction {
             String excMsg = "Currency should not be null";
             throw new NullPointerException(excMsg);
         }
-        if (date == null) {
-            String excMsg = "Date should not be null";
-            throw new NullPointerException(excMsg);
-        }
         this.remark = text;
         this.fundCur = currency;
         this.dateTime = date;
