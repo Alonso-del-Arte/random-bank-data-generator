8b1eaf82ff8b9efe38f12a514adba958b0347195
Alonso del Arte <alonso.delarte@gmail.com>
2025-05-16 07:00:41 -0400

    Partial rewind equals( ) to failing

--- src/accounts/transactions/Comment.java
+++ src/accounts/transactions/Comment.java
@@ -47,9 +47,9 @@ public class Comment extends Transaction {
         if (!this.remark.equals(other.remark)) {
             return false;
         }
-        if (!this.fundCur.equals(other.fundCur)) {
-            return false;
-        }
+//        if (!this.fundCur.equals(other.fundCur)) {
+//            return false;
+//        }
         return this.dateTime.equals(other.dateTime);
     }
 
