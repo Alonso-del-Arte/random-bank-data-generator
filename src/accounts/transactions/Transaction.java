50b12a22d58fb200f928b4838f3fe96d86ba8b37
Alonso del Arte <alonso.delarte@gmail.com>
2025-06-04 07:11:18 -0400

    Passing referential equality test

--- src/accounts/transactions/Transaction.java
+++ src/accounts/transactions/Transaction.java
@@ -16,7 +16,7 @@ public abstract class Transaction {
     // TODO: Write tests for this
     @Override
     public boolean equals(Object obj) {
-        return false;
+        return true;
     }
 
     // TODO: Write tests for this
