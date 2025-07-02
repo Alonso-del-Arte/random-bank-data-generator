c0635a5233a1ba172b410e1a1a7a1efdd3709cc3
Alonso del Arte <alonso.delarte@gmail.com>
2025-07-01 21:42:13 -0400

    Failing not equals diff amount test

--- test/accounts/transactions/WithdrawalTest.java
+++ test/accounts/transactions/WithdrawalTest.java
@@ -87,6 +87,19 @@ class WithdrawalTest {
     }
 
     @Test
+    void testNotEqualsDiffAmount() {
+        int centsAmountA = -RANDOM.nextInt(131072) - 8;
+        int centsAmountB = 2 * centsAmountA - 1;
+        Currency currency = chooseCurrency(2);
+        CurrencyAmount amountA = new CurrencyAmount(centsAmountA, currency);
+        CurrencyAmount amountB = new CurrencyAmount(centsAmountB, currency);
+        LocalDateTime date = LocalDateTime.now().minusDays(RANDOM.nextInt(16));
+        Withdrawal withdrawalA = new Withdrawal(amountA, date);
+        Withdrawal withdrawalB = new Withdrawal(amountB, date);
+        assertNotEquals(withdrawalA, withdrawalB);
+    }
+
+    @Test
     void testConstructorRejectsPositiveWithdrawal() {
         int amountInSubunits = RANDOM.nextInt(65536) + 1;
         Currency currency = chooseCurrency(
