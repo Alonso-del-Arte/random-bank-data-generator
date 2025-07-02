8d16aec8af020e5cf53f52323d5914653dd0f883
Alonso del Arte <alonso.delarte@gmail.com>
2025-07-01 21:43:55 -0400

    Passing not equals diff amount test

--- src/accounts/transactions/Withdrawal.java
+++ src/accounts/transactions/Withdrawal.java
@@ -23,7 +23,7 @@ public class Withdrawal extends Transaction {
     // TODO: Write tests for this
     @Override
     public boolean equals(Object obj) {
-        return obj instanceof Withdrawal;
+        return this == obj;
     }
 
     // TODO: Write tests for this
