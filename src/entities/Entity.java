d5513b17c6a7b10b49f405b00116c7ad204ee01c
Alonso del Arte <alonso.delarte@gmail.com>
2025-07-17 23:20:44 -0400

    Some stubs

--- src/entities/Entity.java
+++ src/entities/Entity.java
@@ -1,4 +1,24 @@
 package entities;
 
+import entities.idnumbers.TaxpayerIdentificationNumber;
+
 public class Entity {
+
+    private String designation;
+
+    // TODO: Write tests for this
+    public String getName() {
+        return "SORRY, NOT IMPLEMENTED YET";
+    }
+
+    // TODO: Write tests for this
+    public TaxpayerIdentificationNumber getTIN() {
+        return null;
+    }
+
+    // TODO: Write tests for this
+    public Entity(String name, TaxpayerIdentificationNumber tin) {
+        //
+    }
+
 }
