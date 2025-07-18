03f34438f0be9e3ab3fcb33bb2186c536155246d
Alonso del Arte <alonso.delarte@gmail.com>
2025-07-17 22:39:50 -0400

    Starting to flesh out

--- src/accounts/Account.java
+++ src/accounts/Account.java
@@ -1,4 +1,26 @@
 package accounts;
 
+import accounts.transactions.Transaction;
+import currency.CurrencyAmount;
+import entities.Entity;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public abstract class Account {
+
+    Entity primaryAccountHolder, secondaryAccountHolder;
+
+    boolean noSecondaryAccountHolderFlag;
+
+    long accountNumber;
+
+    String accountLabel;
+
+    CurrencyAmount accountBalance;
+
+    ArrayList<Transaction> accountHistory;
+
+    Entity accountBeneficiary;
+    
 }
