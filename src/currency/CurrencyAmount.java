8140aca333d8cca3fcd76a5ac2c46963d6df8255
Alonso del Arte <alonso.delarte@gmail.com>
2025-08-12 22:36:14 -0400

    Back to passing all tests so far

--- src/currency/CurrencyAmount.java
+++ src/currency/CurrencyAmount.java
@@ -72,7 +72,14 @@ public class CurrencyAmount {
     }
 
     public CurrencyAmount minus(CurrencyAmount subtrahend) {
-        return this;
+        if (!this.currencyID.equals(subtrahend.currencyID)) {
+            String excMsg = "Amounts " + this + " and " + subtrahend
+                    + " are of mismatched currencies for subtracting";
+            throw new CurrencyConversionNeededException(this, subtrahend,
+                    excMsg);
+        }
+        return new CurrencyAmount(this.totalSubunits - subtrahend.totalSubunits,
+                this.currencyID);
     }
 
     // TODO: Write tests for this
