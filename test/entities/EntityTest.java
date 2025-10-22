29d6c8a26d8b3d7c3a303bedb22046d4c178eb85
Alonso del Arte <alonso.delarte@gmail.com>
2025-10-21 23:59:03 -0400

    Preparing to write first test

--- test/entities/EntityTest.java
+++ test/entities/EntityTest.java
@@ -1,8 +1,31 @@
 package entities;
 
+import java.util.Random;
+
 import static org.junit.jupiter.api.Assertions.*;
+
+import entities.idnumbers.TaxpayerIdentificationNumber;
 import org.junit.jupiter.api.Test;
 
 class EntityTest {
+
+    public static final Random RANDOM = new Random();
+
+    private static String makeName() {
+        int len = RANDOM.nextInt(3, 12);
+        char[] characters = new char[len];
+        for (int i = 0; i < len; i++) {
+            characters[i] = (char) RANDOM.nextInt('a', 123);
+        }
+        return new String(characters);
+    }
+
+    private class EntityImpl extends Entity {
+
+        EntityImpl(String name, TaxpayerIdentificationNumber tin) {
+            super(name, tin);
+        }
+
+    }
 
 }
