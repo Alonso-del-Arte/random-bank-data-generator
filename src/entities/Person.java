a4b0bc52c3001c84ad504ba97262bb8fd4a57e83
Alonso del Arte <alonso.delarte@gmail.com>
2025-10-23 00:08:12 -0400

    Refactored by deleting getter overrides

--- src/entities/Person.java
+++ src/entities/Person.java
@@ -4,28 +4,12 @@ import entities.idnumbers.SocialSecurityNumber;
 
 public class Person extends Entity {
 
-    private final String moniker;
-
-    private final SocialSecurityNumber socSecNum;
-
-    @Override
-    public String getName() {
-        return this.moniker;
-    }
-
-    @Override
-    public SocialSecurityNumber getTIN() {
-        return this.socSecNum;
-    }
-
     public Person(String name, SocialSecurityNumber ssn) {
         super(name, ssn);
         if (name == null || ssn == null) {
             String excMsg = "Name, SSN should not be null";
             throw new NullPointerException(excMsg);
         }
-        this.moniker = name;
-        this.socSecNum = ssn;
     }
 
 }
