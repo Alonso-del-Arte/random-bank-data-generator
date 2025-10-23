888256f0c7e49a915b70c50c5fd934ba6d454f92
Alonso del Arte <alonso.delarte@gmail.com>
2025-10-22 23:30:55 -0400

    Some groundwork for tests

--- test/entities/PersonTest.java
+++ test/entities/PersonTest.java
@@ -1,10 +1,32 @@
 package entities;
 
+import static entities.EntityTest.RANDOM;
 import entities.idnumbers.SocialSecurityNumber;
 
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 class PersonTest {
+
+    private static final String[] FIRST_NAMES = {"Thomas", "Richard", "Harry",
+            "Sally"};
+
+    private static final int NUMBER_OF_FIRST_NAMES = FIRST_NAMES.length;
+
+    private static final String[] LAST_NAMES = {"Thomason", "Richardson",
+            "Harrison", "Sallinen"};
+
+    private static final int NUMBER_OF_LAST_NAMES = LAST_NAMES.length;
+
+    private static String makeName() {
+        int firstNameIndex = RANDOM.nextInt(NUMBER_OF_FIRST_NAMES);
+        int lastNameIndex = RANDOM.nextInt(NUMBER_OF_LAST_NAMES);
+        return FIRST_NAMES[firstNameIndex] + ' ' + LAST_NAMES[lastNameIndex];
+    }
+
+    private static SocialSecurityNumber makeSSN() {
+        return new SocialSecurityNumber(RANDOM
+                .nextInt(SocialSecurityNumber.UPPER_NUMBER_LIMIT));
+    }
 
 }
