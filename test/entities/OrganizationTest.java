813d26c019a4a45d9aa16878e4a0ae1e2b8d5972
Alonso del Arte <alonso.delarte@gmail.com>
2025-10-23 00:53:54 -0400

    Groundwork for tests

--- test/entities/OrganizationTest.java
+++ test/entities/OrganizationTest.java
@@ -1,10 +1,35 @@
 package entities;
 
+import static entities.EntityTest.RANDOM;
 import entities.idnumbers.EmployerIdentificationNumber;
+import entities.idnumbers.SocialSecurityNumber;
+import entities.idnumbers.TaxpayerIdentificationNumber;
+
 
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 class OrganizationTest {
+
+    private static final String[] SUFFIXES = {"Co.", "Corp.", "PLLC", "LLC",
+            "Ltd."};
+
+    private static final int NUMBER_OF_SUFFIXES = SUFFIXES.length;
+
+    private static String makeName() {
+        int len = RANDOM.nextInt(3, 8);
+        char[] characters = new char[len];
+        for (int i = 0; i < len; i++) {
+            characters[i] = (char) RANDOM.nextInt('A', 91);
+        }
+        String acronym = new String(characters);
+        int index = RANDOM.nextInt(NUMBER_OF_SUFFIXES);
+        return acronym + ' ' + SUFFIXES[index];
+    }
+
+    private static EmployerIdentificationNumber makeEIN() {
+        return new EmployerIdentificationNumber(RANDOM
+                .nextInt(EmployerIdentificationNumber.UPPER_NUMBER_LIMIT));
+    }
 
 }
