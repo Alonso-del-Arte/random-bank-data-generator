35a4e8ec64da168ef4d3456f6bbce5261783f994
Alonso del Arte <alonso.delarte@gmail.com>
2025-10-23 23:27:01 -0400

    Refactored getTIN( ) override by deleting it

--- src/entities/Organization.java
+++ src/entities/Organization.java
@@ -4,23 +4,12 @@ import entities.idnumbers.EmployerIdentificationNumber;
 
 public class Organization extends Entity {
 
-    private final String moniker;
-
-    private final EmployerIdentificationNumber num;
-
-    @Override
-    public EmployerIdentificationNumber getTIN() {
-        return this.num;
-    }
-
     public Organization(String name, EmployerIdentificationNumber ein) {
         super(name, ein);
         if (name == null || ein == null) {
             String excMsg = "Name, EIN should not be null";
             throw new NullPointerException(excMsg);
         }
-        this.moniker = name;
-        this.num = ein;
     }
 
 }
