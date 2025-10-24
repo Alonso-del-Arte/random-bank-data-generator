f84dacd7e2b7e235ced2c75087e1bd55c470e32b
Alonso del Arte <alonso.delarte@gmail.com>
2025-10-23 23:34:43 -0400

    Back to passing all tests so far

--- src/entities/Entity.java
+++ src/entities/Entity.java
@@ -17,10 +17,10 @@ public abstract class Entity {
     }
 
     public Entity(String name, TaxpayerIdentificationNumber tin) {
-//        if (name == null || tin == null) {
-//            String excMsg = "Name, TIN should not be null";
-//            throw new NullPointerException(excMsg);
-//        }
+        if (name == null || tin == null) {
+            String excMsg = "Name, TIN should not be null";
+            throw new NullPointerException(excMsg);
+        }
         this.designation = name;
         this.payerID = tin;
     }
