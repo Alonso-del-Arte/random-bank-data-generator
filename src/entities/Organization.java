1af9b9d1853c9ad1f1b063c5e2ae97a98062b06c
Alonso del Arte <alonso.delarte@gmail.com>
2025-10-23 23:38:06 -0400

    Refactored constructor to remove redundant null checks

--- src/entities/Organization.java
+++ src/entities/Organization.java
@@ -6,10 +6,6 @@ public class Organization extends Entity {
 
     public Organization(String name, EmployerIdentificationNumber ein) {
         super(name, ein);
-        if (name == null || ein == null) {
-            String excMsg = "Name, EIN should not be null";
-            throw new NullPointerException(excMsg);
-        }
     }
 
 }
--- src/entities/Person.java
+++ src/entities/Person.java
@@ -6,10 +6,6 @@ public class Person extends Entity {
 
     public Person(String name, SocialSecurityNumber ssn) {
         super(name, ssn);
-        if (name == null || ssn == null) {
-            String excMsg = "Name, SSN should not be null";
-            throw new NullPointerException(excMsg);
-        }
     }
 
 }
