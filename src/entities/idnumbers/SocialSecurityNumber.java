cec5bc23ca300430ca5129678e5327a974ca3072
Alonso del Arte <alonso.delarte@gmail.com>
2025-11-05 01:15:40 -0500

    Refactored correctSSNDashPlacement( ) per IntelliJ IDEA hint

--- src/entities/idnumbers/SocialSecurityNumber.java
+++ src/entities/idnumbers/SocialSecurityNumber.java
@@ -22,7 +22,7 @@ public class SocialSecurityNumber extends TaxpayerIdentificationNumber {
     public static final int UPPER_NUMBER_LIMIT = 1000000000;
 
     static boolean correctSSNDashPlacement(String s) {
-        if (s.length() < 11 || s.length() > 11) {
+        if (s.length() != 11) {
             return false;
         }
         return (s.indexOf('-') == 3) && (s.indexOf('-', 4) == 6)
