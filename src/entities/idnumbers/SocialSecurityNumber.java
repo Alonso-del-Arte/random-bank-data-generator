3c5ee91ee65b4c66114ff2be9dd4b3dd904df3d4
Alonso del Arte <alonso.delarte@gmail.com>
2025-11-13 23:59:40 -0500

    Passing equals( ) test

--- src/entities/idnumbers/SocialSecurityNumber.java
+++ src/entities/idnumbers/SocialSecurityNumber.java
@@ -52,6 +52,15 @@ public class SocialSecurityNumber extends TaxpayerIdentificationNumber {
 
     // TODO: Write tests for this
     @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null) return false;
+        if (!(this.getClass().equals(obj.getClass()))) return false;
+        return this.num == ((SocialSecurityNumber) obj).num;
+    }
+
+    // TODO: Write tests for this
+    @Override
     public int hashCode() {
         return 0;
     }
