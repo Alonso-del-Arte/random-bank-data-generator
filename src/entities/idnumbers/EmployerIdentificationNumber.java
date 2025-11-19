226edaa192fcba6dae6142fb7c720cee137a41e9
Alonso del Arte <alonso.delarte@gmail.com>
2025-11-18 23:59:19 -0500

    Passing not equals diff class test

--- src/entities/idnumbers/EmployerIdentificationNumber.java
+++ src/entities/idnumbers/EmployerIdentificationNumber.java
@@ -34,7 +34,7 @@ public class EmployerIdentificationNumber extends TaxpayerIdentificationNumber {
     // TODO: Write tests for this
     @Override
     public boolean equals(Object obj) {
-        return obj != null;
+        return obj instanceof EmployerIdentificationNumber;
     }
 
     // TODO: Write tests for this
