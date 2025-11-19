0fb2540b7311279e3c1d10d13c2b02c055908fd0
Alonso del Arte <alonso.delarte@gmail.com>
2025-11-19 14:03:37 -0500

    Passing hashCodeOffset( ) test

--- src/entities/idnumbers/EmployerIdentificationNumber.java
+++ src/entities/idnumbers/EmployerIdentificationNumber.java
@@ -23,7 +23,7 @@ public class EmployerIdentificationNumber extends TaxpayerIdentificationNumber {
     // TODO: Write tests for this
     @Override
     int hashCodeOffset() {
-        return 0;
+        return 2;
     }
 
     // TODO: Write tests for this
