b444a5349a317285ee5d10a777b758ca757eaa4e
Alonso del Arte <alonso.delarte@gmail.com>
2025-11-19 23:57:30 -0500

    Passing referential equality test

--- src/entities/idnumbers/TaxpayerIdentificationNumber.java
+++ src/entities/idnumbers/TaxpayerIdentificationNumber.java
@@ -34,7 +34,7 @@ public abstract class TaxpayerIdentificationNumber implements Serializable {
     // TODO: Write tests for this
     @Override
     public boolean equals(Object obj) {
-        return false;
+        return true;
     }
 
     // TODO: Write tests for this
