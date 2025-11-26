c6fd4846e5dbd16650e52f54af7a584f1b952a6a
Alonso del Arte <alonso.delarte@gmail.com>
2025-11-25 22:41:47 -0500

    Passing test that TINs are distinct

--- test/entities/EntityProvider.java
+++ test/entities/EntityProvider.java
@@ -1,12 +1,13 @@
 package entities;
 
 import entities.idnumbers.SocialSecurityNumber;
+import static entities.PersonTest.makeSSN;
 
 public class EntityProvider {
 
     // TODO: Write tests for this
     public static Entity makeEntity() {
-        return new Person("John Q. Public", new SocialSecurityNumber(1));
+        return new Person("John Q. Public", makeSSN());
     }
 
 }
