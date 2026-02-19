b7937574eff8f3b7cbcbdaa9847bfc238c7e1ddb
Alonso del Arte <alonso.delarte@gmail.com>
2026-02-18 22:56:07 -0500

    Preparing to write tests

--- test/accounts/transactions/fees/FeeTest.java
+++ test/accounts/transactions/fees/FeeTest.java
@@ -1,7 +1,23 @@
 package accounts.transactions.fees;
 
+import currency.CurrencyAmount;
+import currency.CurrencyChooser;
+
+import java.util.Currency;
+import java.util.Random;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class FeeTest {
+
+    private static final int MAXIMUM_FEE_IN_SUBUNITS = 10000;
+
+    static final Random RANDOM = new Random(~System.currentTimeMillis() << 4);
+
+    CurrencyAmount makeFeeAmount() {
+        int subunits = -RANDOM.nextInt(MAXIMUM_FEE_IN_SUBUNITS) - 1;
+        Currency currency = CurrencyChooser.chooseCurrency();
+        return new CurrencyAmount(subunits, currency);
+    }
 
 }
