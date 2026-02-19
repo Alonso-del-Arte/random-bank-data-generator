690dfd1fa620f43d53c62d28f45443ec25eed204
Alonso del Arte <alonso.delarte@gmail.com>
2026-02-18 23:31:36 -0500

    Failing getAmount( ) test

--- test/accounts/transactions/fees/MinimumBalanceFeeTest.java
+++ test/accounts/transactions/fees/MinimumBalanceFeeTest.java
@@ -1,8 +1,26 @@
 package accounts.transactions.fees;
 
+import currency.CurrencyAmount;
+
+import java.time.LocalDateTime;
+
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 class MinimumBalanceFeeTest {
+
+    private static final int DEFAULT_ADVISORY = 10000;
+
+    @Test
+    void testGetAmount() {
+        System.out.println("getAmount");
+        CurrencyAmount expected = FeeTest.makeFeeAmount();
+        CurrencyAmount advisory = new CurrencyAmount(DEFAULT_ADVISORY,
+                expected.getCurrency());
+        LocalDateTime now = LocalDateTime.now();
+        Fee instance = new MinimumBalanceFee(expected, advisory, now);
+        CurrencyAmount actual = instance.getAmount();
+        assertEquals(expected, actual);
+    }
 
 }
